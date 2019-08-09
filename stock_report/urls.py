870b0a3c1df0194ba2a3b179f3a87474857938c4
Andrii Bukhanenko <andreybukhanenko@gmail.com>
2019-08-09 22:36:18 +0300

    new logic

--- stock_report/urls.py
+++ stock_report/urls.py
@@ -3,13 +3,14 @@ from . import views
 from .views import List
 
 urlpatterns = [
-
-    url(r'^add-car/$', views.add_car),
+    url(r'^list/$', List.as_view(), name='List-view'),
+    url(r'form/$', views.form),
+    url(r'add-car/$', views.add_car),
     url(r'^add-auto-parts-type/$', views.add_auto_parts_type),
     url(r'^add-auto-parts/$', views.add_auto_parts),
     url(r'^add-auto-parts-warehouse/$', views.add_auto_parts_warehouse),
-    #url(r'', views.form),
-    url(r'', List.as_view(), name=';ist-view'),
+    url(r'^$',views.start_page),
+
 ]
 
 
