666c74e1ab191c219311802a8c2057b1fccb56f5
Andrii Bukhanenko <andreybukhanenko@gmail.com>
2019-08-10 21:04:26 +0300

    new logic

--- auto_app/models.py
+++ auto_app/models.py
@@ -8,17 +8,32 @@ class Car(models.Model):
     car_model = models.CharField(max_length=200, verbose_name="Модель автомобиля")
     car_year = models.IntegerField(verbose_name="Год выпуска")
 
+    def __str__(self):
+        return self.car_brand.title()+" "+self.car_model.title()+" "+str(self.car_year)
+
 class AutoPartsType(models.Model):
     parts_type = models.CharField(max_length=200, verbose_name="Тип автозапчасти")
+
+    def __str__(self):
+        return str(self.parts_type.title())
 
 class AutoParts(models.Model):
     parts_name = models.CharField(max_length=200, verbose_name="Название автозапчасти")
     car_model = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name="Модель автомобиля")
-    part_type = models.ForeignKey(AutoPartsType, on_delete=models.CASCADE, verbose_name="Тип автозапчасти")
+    parts_type = models.ForeignKey(AutoPartsType, on_delete=models.CASCADE, verbose_name="Тип автозапчасти")
+
+    def __str__(self):
+        return self.parts_name.title()+" ("+ str(self.parts_type.parts_type.title())+" | "+self.car_model.car_brand.title()+" "\
+               + self.car_model.car_model+" "+str(self.car_model.car_year) +")"
 
 class AutoPartsWarehouse(models.Model):
     parts_name = models.OneToOneField(AutoParts, on_delete=models.CASCADE, primary_key=True,
                                       verbose_name="Название автозапчасти")
     numbers_of_pats = models.PositiveIntegerField(verbose_name="Количество запчастей")
     price_of_parts = models.PositiveIntegerField(verbose_name="Цена 1й запчати")
+
+    def __str__(self):
+        return self.parts_name.parts_name.title()+" ("+ str(self.parts_name.parts_type.parts_type.title())+" | "+self.parts_name.car_model.car_brand.title()+" "\
+               + self.parts_name.car_model.car_model+" "+str(self.parts_name.car_model.car_year) +")"+" : "+str(self.numbers_of_pats)+"шт., "+str(self.price_of_parts)+" DKK"
+
 
