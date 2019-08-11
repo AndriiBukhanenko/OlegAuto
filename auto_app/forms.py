7a9f3141c60e889b7da717cfe0169b0077a0156c
Andrii Bukhanenko <andreybukhanenko@gmail.com>
2019-08-11 22:58:28 +0300

    new logic

--- auto_app/forms.py
+++ auto_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from . import models
+from django.utils.safestring import mark_safe
 from .models import AutoPartsWarehouse
 from .models import AutoParts
 from .models import AutoPartsType
@@ -8,6 +9,10 @@ from .models import Car
 
 
 class CarForm(forms.ModelForm):
+    car_brand = forms.CharField(label= mark_safe("<strong>Марка автомобиля</strong>"), widget=forms.TextInput(attrs={'class': 'myfield', 'name':'car_brand'}))
+    car_model = forms.CharField(label=mark_safe("<strong>Модель автомобиля</strong>"),
+                    widget=forms.TextInput(attrs={'class': 'myfield', 'name': 'car_brand'}))
+    car_year = forms.IntegerField(label=mark_safe("<strong>Год выпуска</strong>"), widget=forms.TextInput(attrs={'class': 'myfield', 'name': 'car_brand'}))
     class Meta:
         model = Car
         field = ["car_brand", "car_model", "car_year"]
