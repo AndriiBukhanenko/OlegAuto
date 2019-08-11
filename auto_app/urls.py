c7aa9fd5348c6d7ef7a109c0c818640580805778
Andrii Bukhanenko <andreybukhanenko@gmail.com>
2019-08-11 22:58:34 +0300

    new logic

--- auto_app/urls.py
+++ auto_app/urls.py
@@ -5,6 +5,7 @@ from .views import List
 urlpatterns = [
     url(r'^list/$', List.as_view(), name='List-view'),
     url(r'form/$', views.form),
+    url(r'add-car-view/$', views.add_car_view),
     url(r'add-car/$', views.add_car),
     url(r'^add-auto-parts-type/$', views.add_auto_parts_type),
     url(r'^add-auto-parts/$', views.add_auto_parts),
